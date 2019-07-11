e4f417a07584482824d870cc8af44acaa752f086
Aditi Kumari <aditi12395@gmail.com>
2019-07-11 11:37:17 -0700

    test1

--- GlobalLocal.py
+++ GlobalLocal.py
@@ -1,17 +1,17 @@
 f = 101
 g = 123
-print (f)
+print(f)
 
 
 # Global vs.local variables in functions
 def someFunction():
     global f
     g = 23
-    print (f, g)
+    print(f, g)
     f = u"changing global variable"
 
 
 someFunction()
-print "f, g"
+print(f, g)
 
 # To use global declare the variable with global
